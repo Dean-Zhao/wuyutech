21d941c2f8d39e2b7c57528d5707fd27484d7976
Dean-Zhao <zhaowei656@126.com>
2017-09-09 09:58:57 +0800

    update

---
+++ mysqltest.js
@@ -0,0 +1,24 @@
+// var  mysql=require('mysql');
+// var url=require('url');
+// var client=mysql.createConnection({
+//         host:'192.168.0.120',
+//         user:'root',
+//         password:'PASSWORD',
+// });
+// client.connect();
+// client.query('use wuyutech');
+// var http=require('http');
+// http.createServer(
+// function(req,res) {
+// //   client.query('select * from fankui',function(err,data,fil) {
+// //                 for(var i=0;i<data.length;i++)
+// //                 {
+// //                         da+=data[i].id+" "+data[i].name+" "+data[i].age;
+// //                 }
+// //                 res.write(da);
+// //                 res.end();        
+// // });
+// res.writeHead(200,{"Content-Type":'text/plain','charset':'utf-8','Access-Control-Allow-Origin':'*','Access-Control-Allow-Methods':'PUT,POST,GET,DELETE,OPTIONS'});
+//  console.log(res);
+// }).listen(3000);
+
